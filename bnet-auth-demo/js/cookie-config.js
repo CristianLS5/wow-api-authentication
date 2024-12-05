e8c10fb8f7c3a58fba891d9e674a86cf90cf3eb3
CristianLS5 <cristianlopezc4@gmail.com>
2024-12-05 14:28:26 +0100

    fix cookie load

--- bnet-auth-demo/js/cookie-config.js
+++ bnet-auth-demo/js/cookie-config.js
@@ -1,7 +1,28 @@
-CookieConsent.run({
-    root: 'body',
-    autoShow: true,
-    delay: 0,
+import * as CookieConsent from 'vanilla-cookieconsent/dist/core/cookieconsent-core.esm.js';
+
+const cc = CookieConsent;
+
+cc.run({
+    categories: {
+        necessary: {
+            enabled: true,
+            readOnly: true
+        },
+        session: {
+            enabled: true,
+            readOnly: true
+        },
+        analytics: {}
+    },
+
+    cookie: {
+        name: 'cc_cookie',
+        domain: location.hostname,
+        path: '/',
+        sameSite: 'Lax',
+        expiresAfterDays: 365
+    },
+
     guiOptions: {
         consentModal: {
             layout: 'box',
@@ -16,24 +37,7 @@ CookieConsent.run({
             flipButtons: false
         }
     },
-    categories: {
-        necessary: {
-            enabled: true,
-            readOnly: true
-        },
-        analytics: {},
-        session: {
-            enabled: true,
-            readOnly: true
-        }
-    },
-    cookie: {
-        name: 'cc_cookie',
-        domain: location.hostname,
-        path: '/',
-        sameSite: 'Lax',
-        expiresAfterDays: 365
-    },
+
     language: {
         default: 'en',
         translations: {
@@ -44,8 +48,7 @@ CookieConsent.run({
                     acceptAllBtn: 'Accept all',
                     acceptNecessaryBtn: 'Accept necessary only',
                     showPreferencesBtn: 'Manage preferences',
-                    closeIconLabel: 'Close modal',
-                    revision: 1
+                    closeIconLabel: 'Close modal'
                 },
                 preferencesModal: {
                     title: 'Cookie Preferences',
@@ -53,7 +56,6 @@ CookieConsent.run({
                     acceptNecessaryBtn: 'Accept necessary only',
                     savePreferencesBtn: 'Save preferences',
                     closeIconLabel: 'Close modal',
-                    serviceCounterLabel: 'Service|Services',
                     sections: [
                         {
                             title: 'Cookie Usage',
